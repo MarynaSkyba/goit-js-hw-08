a7a0689393dee4dc6a4dda966f094a5fece9cd12
Maryna <marynaskyba@gmail.com>
2021-06-07 20:24:01 +0200

    add remove modal

--- code.js
+++ code.js
@@ -5,13 +5,10 @@ function createGallery(images) {
   return images.map(({preview, original, description}) => {
     return `
     <li class="gallery__item">
-    <a
-      class="gallery__link"
-      href="${original}"
-    >
+    
       <img
         class="gallery__image"
-        src="${preview}"
+        // src="${preview}"
         data-source="${original}"
         alt="${description}"
       />
@@ -24,3 +21,38 @@ function createGallery(images) {
   const mainGallery = document.querySelector('.js-gallery');
   const imageMarkup =   createGallery(images);
   mainGallery.insertAdjacentHTML('beforeend', imageMarkup);
+
+  const openModal = document.querySelector('.js-lightbox')
+
+  
+  mainGallery.addEventListener('click', onImageClick)
+
+  function onImageClick (event) {
+    const galleryTarget = event.target.classList.contains('gallery__item')
+    if (!galleryTarget){
+ 
+ openModal.classList.add('is-open');
+
+  }};
+
+  const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
+
+closeModalBtn.addEventListener('click', onCloseModalBtn)
+function onCloseModalBtn () {
+  openModal.classList.remove('is-open');
+}
+
+// const changeScrImage = document.querySelector('.lightbox__image')
+// console.log(changeScrImage)
+
+//   function changeScrImageFn (images) {
+// images.map.forEach(image => {
+//   changeScrImage.scr = "${images.original}";
+// });
+//   }
+
+
+
+
+
+
