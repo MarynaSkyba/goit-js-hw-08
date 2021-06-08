42849234a5a753e06c66dc626dd3ebfbac9dd093
Maryna <marynaskyba@gmail.com>
2021-06-08 11:18:06 +0200

    update hw8

--- code.js
+++ code.js
@@ -5,25 +5,28 @@ function createGallery(images) {
   return images.map(({preview, original, description}) => {
     return `
     <li class="gallery__item">
-    
+    <a
+    class="gallery__link"
+    href="#"
+  >
       <img
         class="gallery__image"
-        // src="${preview}"
+        src="${preview}"
         data-source="${original}"
         alt="${description}"
       />
     </a>
   </li>`
     }).join('');
-    }
+    };
   
     
   const mainGallery = document.querySelector('.js-gallery');
-  const imageMarkup =   createGallery(images);
+  const imageMarkup =  createGallery(images);
   mainGallery.insertAdjacentHTML('beforeend', imageMarkup);
 
   const openModal = document.querySelector('.js-lightbox')
-
+  const changeScrImage = document.querySelector('.lightbox__image')
   
   mainGallery.addEventListener('click', onImageClick)
 
@@ -32,24 +35,20 @@ function createGallery(images) {
     if (!galleryTarget){
  
  openModal.classList.add('is-open');
-
+ changeScrImage.src = event.target.dataset.source;
+ changeScrImage.alt = event.target.alt;
   }};
 
   const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
 
-closeModalBtn.addEventListener('click', onCloseModalBtn)
-function onCloseModalBtn () {
-  openModal.classList.remove('is-open');
-}
+ closeModalBtn.addEventListener('click', onCloseModalBtn)
+    function onCloseModalBtn () {
+    openModal.classList.remove('is-open');
+    changeScrImage.src = '';
+    changeScrImage.alt = '';
+  };
 
-// const changeScrImage = document.querySelector('.lightbox__image')
-// console.log(changeScrImage)
 
-//   function changeScrImageFn (images) {
-// images.map.forEach(image => {
-//   changeScrImage.scr = "${images.original}";
-// });
-//   }
 
 
 
